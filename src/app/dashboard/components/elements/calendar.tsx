b610c165c038bf21aa47f981cc59932991256ffa
Shogunkayo <pyrotourface@gmail.com>
2023-07-13 02:47:17 +0530

    finished calendar frontend

--- src/app/dashboard/components/elements/calendar.tsx
+++ src/app/dashboard/components/elements/calendar.tsx
@@ -1,21 +1,23 @@
 'use client'
 
-import { add, eachDayOfInterval, endOfMonth, endOfWeek, format, isEqual, parse, startOfMonth, startOfToday } from 'date-fns'
-import React, { useState } from 'react'
+import { add, eachDayOfInterval, endOfMonth, endOfWeek, format, getDay, isEqual, isSameMonth, parse, startOfMonth, startOfToday, startOfWeek } from 'date-fns'
+import { MouseEvent, useState } from 'react'
 import {AiFillCaretDown} from 'react-icons/ai'
 import styles from '../topbar.module.scss';
-
+import Success from '@/assets/success.png'
+import Image from 'next/image';
+import {MdDoneOutline} from 'react-icons/md'
 type Props = {}
 
 const Calendar = (props: Props) => {
-    const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+    const days = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
     const today = startOfToday()
     const [selectedDay, setSelectedDay] = useState(today)
     const [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
     const [isOpen, setIsOpen] = useState(false)
     
     let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
-    let newDays = eachDayOfInterval({ start: firstDayCurrentMonth, end: endOfWeek(endOfMonth(firstDayCurrentMonth))})
+    let newDays = eachDayOfInterval({ start: startOfWeek(firstDayCurrentMonth, {weekStartsOn: 1}), end: endOfWeek(endOfMonth(firstDayCurrentMonth), {weekStartsOn: 1})})
     
     const nextMonth = () => {
         const firstDayNextMonth = add(firstDayCurrentMonth, {months: 1})
@@ -31,31 +33,46 @@ const Calendar = (props: Props) => {
         return false
     }
 
+    const finishTask = (e: MouseEvent<HTMLDivElement>) => {
+        console.log(e.currentTarget.id)
+    }
+
+    const tasks = [{'id': 1, 'title': 'Hehehehaw dsjakdb sad bsuadb sad bsaui dbas i', 'category': 'Grrrr', 'color': '#f45555'},
+                {'id': 2, 'title': 'Hehehehaw', 'category': 'Grrrr', 'color': '#f45555'},
+                {'id': 3, 'title': 'Hehehehaw', 'category': 'Grrrr', 'color': '#f45555'},
+                {'id': 4, 'title': 'Hehehehaw', 'category': 'Grrrr', 'color': '#f45555'},
+                {'id': 5, 'title': 'Hehehehaw', 'category': 'Grrrr', 'color': '#f45555'}]
+
     return (
         <>
-            <div className={`${styles['calendar-closed']} ${isOpen ? styles['calendar-top-open'] : ''}`} onClick={() => setIsOpen(!isOpen)}>
-                <h4>{format(today, "EEE, dd MMM")}</h4>
+            <div className={`${styles['calendar-closed']} ${isOpen ? styles['calendar-top-open'] : ''}`} onClick={() => {setIsOpen(!isOpen); setCurrentMonth(format(today, 'MMM-yyyy')); setSelectedDay(today)}}>
+                <p>{format(today, "EEE, dd MMM")}</p>
                 <div><AiFillCaretDown size={24}></AiFillCaretDown></div>
             </div>
             {isOpen && (
                 <div className={styles['calendar-open']}>
-                    <div className={styles['calendar-head']}>
-                        <h4>{format(firstDayCurrentMonth, 'MMM yyyy')}</h4>
-                        <button onClick={nextMonth}>{'>'}</button>
+                    <div className={styles['calendar-head-left']}>
+                        <h4>{format(firstDayCurrentMonth, 'MMMM yyyy')}</h4>
                         <button onClick={prevMonth}>{'<'}</button>
+                        <button onClick={nextMonth}>{'>'}</button>
                     </div>
+
+                    <div className={styles['calendar-head-right']}>
+                        <h4>Tasks due on {format(selectedDay, 'MMMM dd, yyyy')}</h4>
+                    </div>
+
                     <div className={styles['calendar-left']}>
                         <div className={styles['days']}>
                             {days.map((day, i) => (<button className='button-default no-hover' key={i}>{day}</button>))}
                         </div>
                         <div className={styles['dates']}>
-                            {newDays.map((day) => (
-                                <div key={day.toString()}>
+                            {newDays.map((day, dayIdx) => (
+                                <div key={day.toString()} style={{'gridColumnStart': ((getDay(day) + 6) % 7) + 1}}>
                                     <button className={`button-default no-hover 
                                         ${isEqual(day, today) ? styles.today : ''} 
                                         ${isEqual(day, selectedDay) ? styles['selected-date'] : ''}
-                                        ${hasTasks(day) ? styles['task-indicator'] : ''}`}
-                                         
+                                        ${hasTasks(day) ? styles['task-indicator'] : ''}
+                                        ${isSameMonth(day, firstDayCurrentMonth) ? '' : styles['diff-month']}`} 
                                         onClick={() => setSelectedDay(day)}
                                     >
                                         <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'dd')}</time>
@@ -64,8 +81,23 @@ const Calendar = (props: Props) => {
                             ))}
                         </div>
                     </div>
-                    <div className={styles['calendar-right']}>
 
+                    <div className={styles['calendar-right']}>
+                        {tasks.length === 0 && (
+                            <div className={styles['tasks-empty']}>
+                                <Image src={Success} alt='success' width={320} height={320}></Image>
+                                <p>No tasks left!</p>
+                            </div>
+                        )}
+                        <div className={styles['tasks']}>
+                            {tasks.length !== 0 && tasks.map((task) => (
+                                <div key={task['id']} className={styles['task-card']}>
+                                    <h4>{task['title']}</h4>
+                                    <p style={{'backgroundColor': task['color']}}>{task['category']}</p>
+                                    <div id={task['id'].toString()} onClick={finishTask}><MdDoneOutline size={24}></MdDoneOutline></div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             )}

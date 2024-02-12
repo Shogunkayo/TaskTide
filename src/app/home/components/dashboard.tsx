97fdfa6c1e435a1e1dca38c9e2c258b11d6769ed
Shogunkayo <pyrotourface@gmail.com>
2024-02-12 17:48:53 +0530

    modif

--- src/app/home/components/dashboard.tsx
+++ src/app/home/components/dashboard.tsx
@@ -1,15 +1,20 @@
 import HighPriority from "./elements/highPriority"
 import ProdGraphs from "./elements/prodGraphs"
 import Upcomming from "./elements/upcomming"
+import styles from './dashboard.module.scss'
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
     return (
-        <div>
-            <div><Upcomming></Upcomming></div>
-            <div><ProdGraphs></ProdGraphs></div>
-            <div><HighPriority></HighPriority></div>
+        <div className={styles['dashboard-container']}>
+            <div className={styles['top']}>
+                <Upcomming></Upcomming>
+            </div>
+            <div className={styles['bottom']}>
+                <ProdGraphs></ProdGraphs>
+                <HighPriority></HighPriority>
+            </div>
         </div>
     )
 }
--- src/app/home/components/elements/prodGraphs.tsx
+++ src/app/home/components/elements/prodGraphs.tsx
@@ -1,8 +1,60 @@
+import { RootState } from "@/app/redux/store"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import styles from './prodGraphs.module.scss'
+
 type Props = {}
 
 const ProdGraphs = (props: Props) => {
+    const tasks = useSelector((state: RootState) => state.task.tasks)
+    const categories = useSelector((state: RootState) => state.task.categories)
+    const complete: Array<string> = [];
+    const incomplete: Array<string> = [];
+
+    const [gType, setGType] = useState('bar')
+    const [catType, setCatType] = useState({name: 'Total', id: ''})
+    const [view, setView] = useState('none')
+
+    useEffect(() => {
+        console.log("run")
+        Object.keys(tasks).map((task) => {
+            if (tasks[task].completed)
+                complete.push(task)
+            else
+                incomplete.push(task)
+        })
+
+    }, [tasks])
+
     return (
-        <div>ProdGraphs</div>
+        <div className={styles['graph-comp-container']}>
+            <div className={styles['graph-head']}>
+                <div className={styles['graph-choose']}>
+                    <button onClick={() => setView(view === 'none' ?  'plotType' : 'none')}>Plot Type</button>
+                    <button onClick={() => setView(view === 'none' ? 'categories' : 'none')}>Categories</button>
+                </div>
+                <div className={styles['graph-dropdown']}>
+                    {view === 'plotType' && (
+                        <ul>
+                            <li onClick={() => {setGType('bar'); setView('none')}}>Bar</li>
+                            <li onClick={() => {setGType('pie'); setView('none')}}>Pie</li>
+                        </ul>
+                    )}
+                    {view === 'categories' && (
+                        <ul>
+                            <li onClick={() => {setCatType({name: 'Total', id: ''}); setView('none')}}>Total</li>
+                            <li onClick={() => {setCatType({name: 'All', id: ''}); setView('none')}}>All</li>
+                            {Object.keys(categories).map((cat) => (
+                                <li id={cat} key={cat} onClick={() => {setCatType({name: categories[cat].title, id: cat}); setView('none')}}>{categories[cat].title}</li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </div>
+            <div className={styles['graph-container']}>
+                
+            </div>
+        </div>
     )
 }
 
